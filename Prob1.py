e9bdfbde5956828b4ec4670cf2e0ce5dc5c1688a
sagesoto23 <sksoto@willamette.edu>
2024-10-12 15:22:12 -0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -33,10 +33,10 @@ def draw_image():
         gw.add(p)                           # Adds the rectangle to the window
    
     # Following Lines Create the Background Layer of the Union Jack
-    rectangle(0,0,350,h//2.005,"White")           
-    rectangle(0,0,350,h//2.005,"Blue")            
-    rect_rotate(20,-18,350,h//8.5,"White",-30)    
-    rect_rotate(0,175,350,h//8.5,"White",30)      
+    rectangle(0,0,w-450,h//2.005,"White")           
+    rectangle(0,0,w-450,h//2.005,"Blue")            
+    rect_rotate(20,-18,w-400,h//8.5,"White",-30)    
+    rect_rotate(0,175,w-400,h//8.5,"White",30)      
     rectangle(0,0,15,16,"White")                    
     rectangle(0,175,25,25,"White")                  
     
@@ -51,15 +51,15 @@ def draw_image():
     rectangle(0,350,w,h,"Red")              
     
     # Following Lines Complete the Union Jack
-    rect_rotate(0,200,200,h//25,"Red",30)     
-    rect_rotate(0,-3,200,h//25,"Red",-30)
-    rect_rotate(147,100,250,h//25,"Red",30)
-    rect_rotate(175,83,250,h//25,"Red",-30)
+    rect_rotate(0,200,w-600,h//25,"Red",30)     
+    rect_rotate(0,-3,w-600,h//25,"Red",-30)
+    rect_rotate(147,100,w-550,h//25,"Red",30)
+    rect_rotate(175,83,w-550,h//25,"Red",-30)
     rectangle(350,150,450,h//8.15,"White")
     
-    rectangle(0,65,350,h//5.45,"White")           # Two white cross stripes
+    rectangle(0,65,w-450,h//5.45,"White")           # Two white cross stripes
     rectangle(137.5,0,w//10.75,h//2.005,"White")
-    rectangle(0,77.5,350,h//8.5,"Red")            # Two red cross strips
+    rectangle(0,77.5,w-450,h//8.5,"Red")            # Two red cross strips
     rectangle(150,0,w//16,h//2,"Red")
       
     #circle(0,0,w//4,h//5,"Purple")
